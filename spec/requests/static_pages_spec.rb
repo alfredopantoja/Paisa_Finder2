cb5a617f9eb5df86750f0d4d2b2f330437940135
Alfredo Pantoja <chololoy@gmail.com>
2013-05-10 18:38:29 -0500

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,12 +1,35 @@
 require 'spec_helper'
 
 describe "Static pages" do
-	
-	describe "Home page" do
 
-		it "should have the content 'Paisa Finder'" do
-			visit '/static_pages/home'
-			page.should have_content('Paisa Finder')
-		end
-	end		
+  subject { page }
+
+  describe "Home page" do
+    before { visit root_path }
+
+    it { should have_selector('h1',    text: 'Paisa Finder') }
+    it { should have_selector('title', text: full_title('')) }
+    it { should_not have_selector 'title', text: '| Home' }
+  end
+
+  describe "Help page" do
+    before { visit help_path }
+
+    it { should have_selector('h1',    text: 'Help') }
+    it { should have_selector('title', text: full_title('Help')) }
+  end
+
+  describe "About page" do
+    before { visit about_path }
+
+    it { should have_selector('h1',    text: 'About') }
+    it { should have_selector('title', text: full_title('About Us')) }
+  end
+
+  describe "Contact page" do
+    before { visit contact_path }
+
+    it { should have_selector('h1',    text: 'Contact') }
+    it { should have_selector('title', text: full_title('Contact')) }
+  end
 end
